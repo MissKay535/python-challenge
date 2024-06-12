d3c0037f46d4fe12a9e25175174e58ac5a71d520
misskay535 <167699439+MissKay535@users.noreply.github.com>
2024-06-12 15:13:39 -0400

    average change issues

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,6 +13,8 @@ print("Absolute Path:", absolute_csvpath)
 # set variables
 total_months = 0
 prev_rev = 0
+rev_change = 0
+rev = []
 month_of_change = []
 rev_change_list = []
 great_in = ["", 0]
@@ -26,12 +28,11 @@ with open(csvpath) as csvfile:
     for row in csvreader:
 
         #calculating totals, changes, increases, and decreases
-        total_months = total_months + 1
-        total_rev = total_rev + int(row[1])
+        total_months += 1
+        total_rev += int(row[1])
         rev_change = int(row[1]) - prev_rev
         prev_rev = int(row[1])
-        rev_change_list = rev_change_list + [rev_change]
-        month_of_change = month_of_change + [row[0]]
+        month_of_change += [row[0]]
 
         if (rev_change > great_in[1]):
             great_in[0] = row[0]
@@ -41,18 +42,25 @@ with open(csvpath) as csvfile:
             great_de[0] = row[0]
             great_de[1] = rev_change
 
-# find avgerage revenue change
-rev_avg = sum(rev_change_list) / len(rev_change_list)
+# find average revenue change
+rev_change_list = rev_change_list + [rev_change]
+sum = 0
+for num in rev_change_list:
+    sum += num
+average = sum / len(rev_change_list)
+
+
+
 
 # generate findings
 output = (
     f"\nFinancial Analysis\n"
     f"-----------------------\n"
     f"Total Months: {total_months}\n"
-    f"Total Revenue: ${total_rev}\n"
-    f"Average Revenue Change: ${rev_avg}\n"
-    f"Greatest Increase in Revenue: {great_in[0]} (${great_in[1]})\n"
-    f"Greatest Decrease in Revenue: {great_de[0]} (${great_de[1]})\n"
+    f"Total: ${total_rev}\n"
+    f"Average Change: ${average}\n"
+    f"Greatest Increase in Profits: {great_in[0]} (${great_in[1]})\n"
+    f"Greatest Decrease in Profits: {great_de[0]} (${great_de[1]})\n"
 )
 
 # print results
